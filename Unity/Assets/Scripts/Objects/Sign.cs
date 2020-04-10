480552a848cfd4030bedbe78574bf26c6d324721
Adrián González Barbosa <adrian.barbosa@senseitrade.com>
2020-04-11 00:06:22 +0200

    Added door, still needs key and persistence

--- Unity/Assets/Scripts/Objects/Sign.cs
+++ Unity/Assets/Scripts/Objects/Sign.cs
@@ -3,43 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Sign : MonoBehaviour
-{
+public class Sign : Interactable {
+
     public GameObject dialogBox;
     public Text dialogText;
     public string dialog;
-    public bool playerInRange;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
         if(Input.GetKeyDown(KeyCode.Space) && playerInRange)
         {
             if(dialogBox.activeInHierarchy)
             {
                 dialogBox.SetActive(false);
-            } else {
-                dialogText.text = dialog;
+            }else{
                 dialogBox.SetActive(true);
+                dialogText.text = dialog;
             }
         }
-    }
+	}
 
-    void OnTriggerEnter2D(Collider2D other) {
-        if(other.CompareTag("Player")) {
-            playerInRange = true;
-        }
-    }
-    private void OnTriggerExit2D(Collider2D other) {
-        if(other.CompareTag("Player")) {
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            context.Raise();
             playerInRange = false;
             dialogBox.SetActive(false);
         }
     }
+
 }
