9daa152d9e964bc61eca8bb95e2f4b655d61f9dd
Adrián González Barbosa <adrian.barbosa@senseitrade.com>
2020-06-02 02:18:15 +0200

    UPDATE: Censorship
    Censor students names

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+rsync -av ./Web/ ovh01:/var/www/adriangonzalezbarbosa.eu/html/TFMMEI/
+rsync -av Unity/Build/BuildWeb/Build/ ovh01:/var/www/adriangonzalezbarbosa.eu/html/TFMMEI/Unity/Build/BuildWeb/Build/
